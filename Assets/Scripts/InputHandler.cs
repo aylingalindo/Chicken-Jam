2ee3232827b69822f956ed052452ff2e79b272f2
Notalexs <124293070+bosieett@users.noreply.github.com>
2024-01-27 04:50:21 -0600

    Random faces

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem; 
 
 public class InputHandler : MonoBehaviour
 {
-    private Camera _mainCamera; 
+    private Camera _mainCamera;
+
+    public List<Sprite> sprites;
+    public SpriteRenderer spriteR;
 
     private void Awake() {
         _mainCamera = Camera.main;
     }
 
     public void OnClick(InputAction.CallbackContext context){
+        Debug.Log("rayhit");
 
         // si no está inicializada la escena 
         if(!context.started) return;
-
+        Debug.Log("rayhit");
         // checa donde hizo click 
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
 
@@ -28,12 +33,18 @@ public class InputHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log("a");
+        //InvokeRepeating("RandomFace", 0, 6);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void RandomFace(){
+        int index = Random.Range(0,8);
+        spriteR.sprite = sprites[index];
     }
 }
