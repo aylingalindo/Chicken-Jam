3a68b0bcc83895ccf1daccb7825900ce967987cc
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2024-01-26 23:38:06 -0600

    Menu principal y cambio a nivel principal

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Creamos nuestra propia funcion para siempre que se llame nuestro boton
+    public void PlayGame()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       
+    }
+}
