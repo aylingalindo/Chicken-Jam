6e4ca3942c54eb8c0ef4ebd1961b1fee4aea51b4
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2024-01-27 00:39:52 -0600

    Menu Pausa
    Ya fue creado y es posible pausar y despausar con las teclas y click

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,12 +9,13 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
        
     }
 
     public void QuitGame()
     {
+        Debug.Log("Quit");
         Application.Quit();
     }
 }
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public static bool IsGamePaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (IsGamePaused)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0;
+        IsGamePaused = true;
+    }
+
+    public void Continue()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1; 
+        IsGamePaused = false;
+    }
+}
