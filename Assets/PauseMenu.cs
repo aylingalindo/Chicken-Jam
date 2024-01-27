e5bfa0aa347e7115ee80cc71cafe04a98384e9e5
Isaac Espinoza <81329145+Wonderedcoot4@users.noreply.github.com>
2024-01-27 01:16:36 -0600

    Salida a Menu Principal
    La salida al menu principal ya esta hecha

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -46,6 +47,8 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Debug.Log("CargandoMenu...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
     }
 
     public void LoadOptions()
